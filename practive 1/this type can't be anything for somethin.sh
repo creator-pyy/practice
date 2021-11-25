059f6d9e4e91763d6ad7fa3627ef4e1422eea467
creator-pyy <dbsdyd0312@gmail.com>
2021-11-25 14:53:46 +0900

    rrrr

---
+++ practive 1/this type can't be anything for somethin.sh
@@ -0,0 +1,23 @@
+this type can't be anything for something
+dkfjakjfjasdkf
+
+dkfjakjfjasdkffas
+dkfjakjfjasdkfsad
+forasd
+forasdf
+sad
+feiiieiwjigjwr
+gdkfaskdjfkasdf
+ewrwerwer
+wewegwegwe
+weg
+we
+gwe
+g
+we
+gw
+eg
+
+
+ewgwegwegwe
+
